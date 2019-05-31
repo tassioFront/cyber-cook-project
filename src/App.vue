b2ff97724446fae813e9ed001e11fda6ec1e3daa
TassioSense <tassio.jordao@sensenova.com.br>
2019-05-31 18:46:59 -0300

    Entendendo estrutura do projeto, criando componentes e começando o header

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,11 @@
   <div id="app">
     <!-- https://tpc.googlesyndication.com/simgad/16268655339236767166 -->
 
-    <!-- https://cybercook.com.br/assets/mr-pancake/images/logos/cybercook.svg -->
+    <!--Icone https://cybercook.com.br/assets/mr-pancake/images/logos/cybercook.svg -->
 
     <!-- https://img.cybercook.com.br/receitas/791/nhoque-de-batata-doce-2.jpeg?fm=jpeg&w=1024 -->
 
     <Header  heade msg="Welcome to Your Vue.js App" />
-    <Banner banner/> 
     <Recipes recipes/>
     <Feeds feeds/>
 
@@ -16,18 +15,16 @@
 
 <script>
 import Header from './components/Header'
-import Banner from './components/Banner'
-import Feeds from './components/feeds/Feeds'
 import Recipes from './components/recipes/Recipes'
+import Feeds from './components/feeds/Feeds'
 
 
 export default {
   name: 'app',
   components: {
     Header,
-    Banner,
-    Feeds,
-    Recipes
+    Recipes,
+    Feeds
   }
 }
 </script>
@@ -45,12 +42,19 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     box-sizing: border-box;
+    width: 51.88%; 
     max-width: 996px;
+    /* min-width: 500px; */
     margin: 0 auto;
-    display: grid;
-    grid-template-columns: 966px;
-    grid-template-rows: 226px 326px 856px 316px;
+
+    display: flex;
+    flex-direction: column;
+
+
+    /* grid-template-columns: 51.88%;
+    grid-template-rows: 226px 326px 856px 316px; */
     padding: 5px 15px 30px;
+    border: 1px solid orange;
 }
 
   [heade], [banner], [recipes], [feeds] {
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,27 @@
 <template >
   <header>
-    {{ msg }}
-    {{ cc }}
+
+    <img class="banner-1" src="https://tpc.googlesyndication.com/simgad/16268655339236767166" alt="Conheça estes diferentões">    
+
+    <div class="menu">
+      <!-- icon -->
+      <a  href="https://cybercook.com.br/">
+        <img class="icon" src="https://cybercook.com.br/assets/mr-pancake/images/logos/cybercook.svg" alt="Cyber Cook"/> 
+      </a>
+
+      <div class="menu-content">
+      
+        <div class="menu-link">
+
+          <div class="social"></div>
+          <div class="actions"></div>
+        
+        </div>
+      
+        <nav></nav>
+      </div>
+
+    </div>
 
   </header>
 </template>
@@ -9,20 +29,53 @@
 <script>
 
 export default {
-  name: 'Header',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      cc: 'dd'
-    }
-  }
+  
+  
 }
 </script>
 
 
 <style scoped>
+
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+header .banner-1 {
+  margin-bottom: 15px;
+}
+
+.menu {
+  width: 100%;
+  height: 45.57%;
+  display: flex;
+}
+
+.icon {
+  width: 83px;
+  height: 103px;
+} 
+
+.menu-content {
+  flex-grow: 1;
+  border: 1px solid orange;
+  display: flex;
+  flex-direction: column;
+}
+
+.menu-link {
+  flex-grow: 1;
+}
+
+.menu-content nav {
+  flex-grow: 1;
+}
+
+
+
+/* 
 h3 {
   margin: 40px 0 0;
 }
@@ -36,9 +89,7 @@ li {
 }
 a {
   color: #42b983;
-}
+} */
 
-.c1 {
-  display: none;
-}
+
 </style>
